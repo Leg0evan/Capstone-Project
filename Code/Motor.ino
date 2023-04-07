9ad6432c419972075f732fa1ded703e721ee893b
Leg0evan <99287638+Leg0evan@users.noreply.github.com>
2023-04-07 13:34:24 -0600

    Update Motor.ino

--- Code/Motor.ino
+++ Code/Motor.ino
@@ -2,29 +2,40 @@
 
 #define ESC_PIN D2
 
+/*
+Need to attach a switch to turn off the motor
+*/
+
 Servo esc;
-int speed=1;
+int speed;
+
+void takeOff(){
+  speed=0;
+  for(int x=0; x<180; x=x+1){
+    esc.write(speed);
+    Serial.println(speed);
+    speed=speed+1;
+    delay(100);
+  }
+  delay(1000);
+  speed=180;
+  for(int x=180; x>0; x=x-1){
+    esc.write(speed);
+    Serial.println(speed);
+    speed=speed-1;
+    delay(100);
+  }
+  delay(5000);
+}
 
 void setup() 
 {
   esc.attach(ESC_PIN,  1000, 2000);
-  esc.write(0);
   Serial.begin(9600);
+  esc.write(0);
 }
 
 void loop() 
 {
-  for(int x=0; x<181; x=x+1){
-    esc.write(speed);
-    speed=speed+1;
-    delay(100);
-    Serial.println(speed);
-  }
-  for (int x=180; x>-1; x=x-1){
-    esc.write(speed);
-    speed=speed-1;
-    delay(100);
-    Serial.println(speed);
-  }
-  delay(5000);
+takeOff();
 }
