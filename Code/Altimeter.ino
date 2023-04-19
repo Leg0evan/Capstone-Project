a0e4c76ff2926fa0a293c9d49f03c1219701371f
Leg0evan <99287638+Leg0evan@users.noreply.github.com>
2023-04-19 13:29:35 -0600

    Update Altimeter.ino

--- Code/Altimeter.ino
+++ Code/Altimeter.ino
@@ -1,24 +1,42 @@
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
- 
-#define SEALEVELPRESSURE_HPA (1013.25)
- 
-Adafruit_BME280 bme;
- 
-float temperature, humidity, pressure, altitude, og_altitude;             
- 
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
+
+#define BMP_SCK (13)
+#define BMP_MISO (12)
+#define BMP_MOSI (11)
+#define BMP_CS (10)
+
+Adafruit_BMP280 bmp;  // I2C
+
 void setup() {
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
   Serial.begin(115200);
-  delay(100);
-  bme.begin(0x76);
-  altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
-  og_altitude=altitude;
-  Serial.println(altitude);
+  while (!Serial) delay(100);  // wait for native usb
+  unsigned status;
+  status = bmp.begin(0x76);
+  if (!status) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                     "try a different address!"));
+    Serial.print("SensorID was: 0x");
+    Serial.println(bmp.sensorID(), 16);
+    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    Serial.print("        ID of 0x61 represents a BME 680.\n");
+    while (1) delay(10);
+  }
+
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 }
 
 void loop() {
-  altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
-  Serial.println(altitude-og_altitude);
+  Serial.println(bmp.readAltitude(1013.25));
   delay(1000);
 }
